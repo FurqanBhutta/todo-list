8730fcc28942870981156415aa6b0087f42ec162
furqan bhutta <bhutta.furqan9@gmail.com>
2025-02-19 22:53:58 +0500

    hooooooo

--- app.js
+++ app.js
@@ -131,6 +131,6 @@ app.get("/about", (req,res)=>{
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, "0.0.0.0" ,  ()=>{
     console.log("port is listening at port 3000");
 })
